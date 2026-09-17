a23453a0a4a7fb8ce9ea7501cb2cac95ae927417
onurteknik <onurteknikbulut@gmail.com>
2026-09-17 17:00:41 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='ot-parca-barkod-v3';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
